b64a81ce07c4bf2867da199d3dc65abc380aea5e
mggggk <mggggk@gmail.com>
2022-03-05 15:51:44 +0900

    Add controller, thymeleaf template

--- src/main/kotlin/com/example/demo/controller/MainController.kt
+++ src/main/kotlin/com/example/demo/controller/MainController.kt
@@ -1,17 +1,25 @@
-package com.example.demo.com.example.demo.controller
+package com.example.demo.controller
 
-import com.example.demo.com.example.demo.controller.com.example.demo.entity.Book
+import com.example.demo.entity.Book
+import com.example.demo.repository.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 
 @Controller
 class MainController {
+    @Autowired
+    lateinit var bookRepository: BookRepository
+
     @GetMapping("/")
-    fun showUsers(): String{
+    fun showUsers(model: Model): String{
         val book = Book()
         book.title="this is test"
+
+        val books = bookRepository.findAll()
+        model.addAttribute("books", books)
         return "index"
     }
 }
