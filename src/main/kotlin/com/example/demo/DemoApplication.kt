99ed6e8b0bc1d53b41f5414e0d766c8813b31089
mggggk <mggggk@gmail.com>
2022-03-05 15:09:46 +0900

    Add template

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+	println("hello")
+	Thread.sleep(10000)
+	println("hello")
+}
