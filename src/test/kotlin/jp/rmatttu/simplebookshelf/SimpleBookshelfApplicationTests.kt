0c506d609e78b577057b9aa39cdceb4c405bf9e9
mggggk <mggggk@gmail.com>
2022-03-09 23:52:14 +0900

    Add memo

--- src/test/kotlin/jp/rmatttu/simplebookshelf/SimpleBookshelfApplicationTests.kt
+++ src/test/kotlin/jp/rmatttu/simplebookshelf/SimpleBookshelfApplicationTests.kt
@@ -6,8 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class SimpleBookshelfApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+        // TODO: 考慮する
+        // * null
+        // * 空（""）
+        // * max値
+        // * 制御文字
+    }
 
 }
