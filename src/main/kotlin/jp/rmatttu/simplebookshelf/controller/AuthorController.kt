1677545bf3f236e5f44486a355e0953d52f0a43a
mggggk <mggggk@gmail.com>
2022-03-13 13:13:17 +0900

    Add showing books in author page

--- src/main/kotlin/jp/rmatttu/simplebookshelf/controller/AuthorController.kt
+++ src/main/kotlin/jp/rmatttu/simplebookshelf/controller/AuthorController.kt
@@ -1,22 +1,42 @@
 package jp.rmatttu.simplebookshelf.controller
 
+import jp.rmatttu.simplebookshelf.entity.Book
 import jp.rmatttu.simplebookshelf.repository.AuthorRepository
+import jp.rmatttu.simplebookshelf.repository.BookAuthorRepository
+import jp.rmatttu.simplebookshelf.repository.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.server.ResponseStatusException
 
 @Controller
 class AuthorController {
     @Autowired
     lateinit var authorRepository: AuthorRepository
 
+    @Autowired
+    lateinit var bookRepository: BookRepository
+
+    @Autowired
+    lateinit var bookAuthorRepository: BookAuthorRepository
+
     @GetMapping("/author/{id}")
     fun index(@PathVariable id: Int, model: Model): String {
-        val author = authorRepository.findById(id)
-        model["author"] = author.get()
+        val authorRecord = authorRepository.findById(id)
+        if (authorRecord.isEmpty()){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found id: $id")
+        }
+
+        val author = authorRecord.get()
+        val bookRecords = bookAuthorRepository.findByAuthorId(author.id)
+        val books = bookRecords.map { it.book }
+
+        model["author"] = author
+        model["books"] = books
         return "author/author"
     }
 
--- src/main/kotlin/jp/rmatttu/simplebookshelf/repository/BookAuthorRepository.kt
+++ src/main/kotlin/jp/rmatttu/simplebookshelf/repository/BookAuthorRepository.kt
@@ -4,4 +4,5 @@ import jp.rmatttu.simplebookshelf.entity.BookAuthor
 import org.springframework.data.repository.CrudRepository
 
 interface BookAuthorRepository : CrudRepository<BookAuthor, Int> {
+    fun findByAuthorId(authorId: Int): List<BookAuthor>
 }
