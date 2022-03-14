e13ab983a2dca48f45c6848855c5ed4e26689b11
mggggk <mggggk@gmail.com>
2022-03-14 20:00:57 +0900

    Improve BookController

--- src/main/kotlin/jp/rmatttu/simplebookshelf/controller/BookController.kt
+++ src/main/kotlin/jp/rmatttu/simplebookshelf/controller/BookController.kt
@@ -18,6 +18,14 @@ class BookController {
     @Autowired
     lateinit var bookRepository: BookRepository
 
+    private fun getBook(id: Int): Book {
+        val bookRecord = bookRepository.findById(id)
+        if (bookRecord.isEmpty) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found id: $id")
+        }
+        return bookRecord.get()
+    }
+
     @GetMapping("/book/new")
     fun newGetMethod(model: Model): String {
         return "book/new"
@@ -37,24 +45,16 @@ class BookController {
 
     @GetMapping("/book/{id}")
     fun index(@PathVariable id: Int, model: Model): String {
-        val book = bookRepository.findById(id)
-        if (book.isEmpty) {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found id: $id")
-        }
-        model["message"] = book.get().title
-        model["book"] = book.get()
-        model["authors"] = book.get().bookAuthors.map { it.author }
+        val book = getBook(id)
+        model["message"] = book.title
+        model["book"] = book
+        model["authors"] = book.bookAuthors.map { it.author }
         return "book/book"
     }
 
     @GetMapping("book/{id}/edit")
     fun edit(@PathVariable id: Int, model: Model): String {
-        val bookRecord = bookRepository.findById(id)
-        if (bookRecord.isEmpty) {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found id: $id")
-        }
-        val book = bookRecord.get()
-
+        val book = getBook(id)
         model["edited"] = false
         model["book"] = book
         return "book/edit"
@@ -62,18 +62,13 @@ class BookController {
 
     @PostMapping("book/{id}/edit")
     fun editPost(@PathVariable id: Int, @RequestParam title: String, model: Model): String {
-        val bookRecord = bookRepository.findById(id)
-        if (bookRecord.isEmpty) {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Not found id: $id")
-        }
-        val book = bookRecord.get()
+        val book = getBook(id)
 
         // TODO titleのバリデーションを追加、現状、前回同様のタイトル、空文字などが登録できる
         // TODO ログに変更を記録
 
         val editedBook = Book(book.id, title, book.bookAuthors)
         bookRepository.save(editedBook)
-
         model["edited"] = true
         model["book"] = editedBook
         return "book/edit"
